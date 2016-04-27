11af0cfea5c2e028e705ed2e9e6e178d59b22c55
Maximusvrn <max.vrn.minimax@gmail.com>
2016-04-27 19:51:53 +0300

    first

---
+++ Printer/PrinterScreens/Document.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrinterScreens
+{
+	/// <summary>
+	/// Interaction logic for Document.xaml
+	/// </summary>
+	public partial class Document : UserControl
+	{
+		public Document()
+		{
+			this.InitializeComponent();
+		}
+
+        private void d1_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Документ <Расписание.txt>  отправлен на печать";
+
+        }
+
+        private void d2_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Документ <отчет.docx>  отправлен на печать";
+        }
+
+        private void d3_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Документ <123.txt>  отправлен на печать";
+        }
+
+        private void d4_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Документ <инструкция.txt>  отправлен на печать";
+        }
+
+        private void d5_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Документ <стихи.txt>  отправлен на печать";
+        }
+
+        private void d6_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Документ <шаблон.docx>  отправлен на печать";
+        }
+	}
+}
---
+++ Printer/PrinterScreens/photo.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrinterScreens
+{
+	/// <summary>
+	/// Interaction logic for photo.xaml
+	/// </summary>
+	public partial class photo : UserControl
+	{
+		public photo()
+		{
+			this.InitializeComponent();
+		}
+
+        private void p1_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Изображение <Фото1> отправлено на печать";
+        }
+
+        private void p2_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Изображение <Фото2> отправлено на печать";
+        }
+
+        private void p3_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Изображение <Фото3> отправлено на печать";
+        }
+
+        private void p4_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Изображение <Фото4> отправлено на печать";
+        }
+
+        private void p5_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Изображение <Фото5> отправлено на печать";
+        }
+
+        private void p6_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Изображение <Фото6> отправлено на печать";
+        }
+
+        private void p7_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Изображение <Фото7> отправлено на печать";
+        }
+
+        private void p8_Click(object sender, RoutedEventArgs e)
+        {
+            text.Text = "";
+            text.Text = "Изображение <Фото8> отправлено на печать";
+        }
+	}
+}
